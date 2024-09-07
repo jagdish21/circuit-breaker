b270ec745bd8be45cf7b6de2209f2f91bbe1ce88
jagdish7124 <jagdish.koli@aurionpro.com>
2024-09-08 01:49:29 +0530

    Files Added: Circuit breaker library plugin for Node

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-# Circuit Breaker Node
-Circuit breaker library plugin for Node, express and sequelize with working API example.
-
-## Installation
-To install the package, run:
-```bash
-npm install circuit-breaker
-npm i
-```
-
-## Example to use this plugin with Node.js express, SQL server.
-```bash
+/*
+  Author: Jagdish21
+  github: https://github.com/jagdish21
+*/
 const express = require('express');
 const Sequelize = require('sequelize');
 const CircuitBreaker = require('./lib/circuitBreaker');
@@ -18,19 +10,23 @@ const circuitBreakerOptions = require ('./lib/circuitBreakerOptions');
 const app = express();
 const port = 4000;
 
+
+/* Configure Sequelize: Please replace userName, password, hostname and database name with your actual credentials */
 const sequelize = new Sequelize('mssql://userName:password@hostname:1433/database');
 
 async function executeStoredProcedure(mobile) {
     let sqlInlineQuery = ` SELECT * FROM users with(nolock) where mobile='${mobile}'`;
+    /* let sqlStoreProcedure = "EXEC sqlStoreProcedureForGetUser @mobile = :mobile"; */
+    /* You may use inline query and SQL store procedures, replace sqlInlineQuery with sqlStoreProcedure */
     const result = await sequelize.query(sqlInlineQuery, {
         replacements: { mobile }
     });
     return result;
 }
-// Create a instance for circuit breaker
+// Create a circuit breaker instance
 const circuitBreaker = new CircuitBreaker(executeStoredProcedure, circuitBreakerOptions);
 
-// Define a route/API that uses the circuit breaker
+// Define a route that uses the circuit breaker
 app.get('/userdetails', async (req, res) => {
     const { mobile } = req.query;
 
