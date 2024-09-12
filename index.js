1ce3b9a71374a4d6688f26fe26f98f5a2b5cbd53
jagdish7124 <jagdish.koli@aurionpro.com>
2024-09-12 23:37:32 +0530

    Updated mssql connection string, version and added keyword.

--- index.js
+++ index.js
@@ -12,12 +12,26 @@ const port = 4000;
 
 
 /* Configure Sequelize: Please replace userName, password, hostname and database name with your actual credentials */
-const sequelize = new Sequelize('mssql://userName:password@hostname:1433/database');
+const sequelize = new Sequelize({
+    username: 'root',
+    password: 'root',
+    database: 'mydb',
+    host: 'localhost',
+    dialect: 'mssql',
+    port: '1433',
+    dialectOptions: {
+      options: {
+        encrypt: true, // Use this for Azure SQL
+        trustServerCertificate: true, // For self-signed or local development
+      },
+    },
+  });
+// console.log(sequelize.config); //Checking SQL configuration log
 
 async function executeStoredProcedure(mobile) {
-    let sqlInlineQuery = ` SELECT * FROM users with(nolock) where mobile='${mobile}'`;
-    /* let sqlStoreProcedure = "EXEC sqlStoreProcedureForGetUser @mobile = :mobile"; */
     /* You may use inline query and SQL store procedures, replace sqlInlineQuery with sqlStoreProcedure */
+    let sqlInlineQuery = ` SELECT * FROM users with(nolock) where mobile=:mobile`;
+    /* let sqlStoreProcedure = "EXEC sqlStoreProcedureForGetUser @mobile = :mobile"; */
     const result = await sequelize.query(sqlInlineQuery, {
         replacements: { mobile }
     });
@@ -31,7 +45,7 @@ app.get('/userdetails', async (req, res) => {
     const { mobile } = req.query;
 
     try {
-        const result = await circuitBreaker.fire( mobile);
+        const result = await circuitBreaker.fire(mobile);
         res.send(result);
     } catch (error) {
         res.status(500).send('Service unavailable');
